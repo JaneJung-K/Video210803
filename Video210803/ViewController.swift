d5ec7e976922cd9d08d0fb11771d4010245b7a94
JaneJung-K <mg.yang@hupayx.com>
2021-08-03 11:23:05 +0900

    Initial Commit

---
+++ Video210803/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Video210803
+//
+//  Created by Mingeun Yang on 2021/08/03.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
