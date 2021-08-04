5da14002d6ae6f4b03c7fc5eac8d40be3d7c3e1d
JaneJung-K <mg.yang@hupayx.com>
2021-08-04 10:09:33 +0900

    옵셔널 타입으로 변경하기

--- Video210803/View/SecondViewController.swift
+++ Video210803/View/SecondViewController.swift
@@ -7,11 +7,16 @@
 
 import UIKit
 import Kingfisher
+import AVKit
 
 class SecondViewController: UIViewController {
     
     var userIndex: Int?
     var nTitle: String?
+
+    
+    @IBOutlet weak var pictureCollectionView: UICollectionView!
+    @IBOutlet weak var thumnail: UIImageView!
     
     lazy private var videoManager = VideoManager()
     private var videoData: [Video] = []
@@ -19,15 +24,49 @@ class SecondViewController: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.isNavigationBarHidden = false
         
-        videoManager.getVideo { [weak self] response in
-            self?.videoData = response.videos
-
-        }
     }
     
     override func viewDidLoad() {
         self.title = nTitle!
-       
+        
+        setup()
+
+
+        }
+        
+        private func setup() {
+            videoManager.getVideo { [weak self] response in
+
+                self?.videoData = response.videos
+                let url = URL(string: (self?.videoData[self!.userIndex!].image)!)
+                self?.thumnail.kf.setImage(with: url)
+                print(self?.videoData[self!.userIndex!].videoPictures.count)
+                self?.pictureCollectionView.reloadData()
+        }
+        
     }
     
+}
+
+extension SecondViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        let pictureData = self.videoData[self.userIndex!].videoPictures
+        return pictureData.count
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! UserPicture
+        let pictureIndex = self.videoData[self.userIndex!].videoPictures
+        let url = URL(string: (pictureIndex[indexPath.row].picture))
+        cell.userPicture.kf.setImage(with: url)
+        return cell
+        
+    }
+    
+    
+}
+
+class UserPicture: UICollectionViewCell {
+    @IBOutlet weak var userPicture: UIImageView!
 }
